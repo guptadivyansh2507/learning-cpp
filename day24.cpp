a88086464c0b8a1f9dbf7457a867b0b38714ddaf
Divyansh Gupta <guptadivyansh2507@gmail.com>
2026-05-23 19:41:59 +0530

    Implement majority element finder in day24.cpp

---
+++ day24.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int majorityElement(std::vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        return nums[nums.size() / 2];
+    }
+};
